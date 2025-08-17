022d0e9813dde613c68674d5e524d3bbfa9bfc1b
Diganta Kr Banik <digantabanik2000@gmail.com>
2025-08-18 00:33:06 +0530

    feat5: added in memory batching for incoming log metrics

--- kafka/consumer.js
+++ kafka/consumer.js
@@ -3,6 +3,26 @@ const { Kafka } = require('kafkajs');
 const { addDocument } = require('../collector/repository/collector.repository');
 const kafka = new Kafka({ clientId: 'luxi-consumer', brokers: ['localhost:9092'] });
 const consumer = kafka.consumer({ groupId: 'luxi-group' })
+const batch = [];
+const FLUSH_INTERVAL = 600000;
+const BATCH_SIZE = 10;
+
+const flushBatch = async () => {
+    console.log({ batch })
+    if (batch.length === 0) return;
+    const docsToInsert = [...batch];
+    batch = []
+    try {
+        await Promise.all(
+            docsToInsert.map(({ data, index }) =>
+                addDocument(data, index)
+            )
+        );
+        console.log(`Flushed ${docsToInsert.length} docs`);
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 const runConsumer = async () => {
     await consumer.connect();
@@ -11,10 +31,18 @@ const runConsumer = async () => {
         eachMessage: async ({ topic, partition, message }) => {
             const parsed = JSON.parse(message.value.toString());
             const { index, data } = parsed;
-            const response = await addDocument(data, index);
-            console.log(response)
+            batch.push({
+                data, index
+            });
+
+            if (batch.length >= BATCH_SIZE) {
+                await flushBatch();
+            }
+
         }
     });
+
+    setInterval(flushBatch, FLUSH_INTERVAL)
 }
 
 
